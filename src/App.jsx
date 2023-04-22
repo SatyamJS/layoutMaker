0c5c796427533702a2f2b8001da8f344151aead6
Satyam <iamsatyam760@gmail.com>
2023-04-22 20:59:57 +0530

    fixes

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,15 @@ import "./App.css";
 function App() {
   const [elements, setElements] = useState([]);
 
+
   const handleButtonClick = (e) => {
     const buttonName = e.target.innerText.toLowerCase();
     const elementSrc = `${buttonName}.png`;
     const elementRef = React.createRef();
-    const elementClassName = buttonName === "connector" || buttonName === "vertical connector" ? "element no-animation" : "element";
+    const elementClassName = buttonName === "connector" || buttonName === "tbconnector" || buttonName==="btconnector" ? "element no-animation" : "element";
     const newElement = (
       <Draggable>
-        <div className={elementClassName}>
+        <div className={elementClassName +" "+elementSrc.split(".")[0]} >
           <img src={elementSrc} alt={buttonName} ref={elementRef} />
           <div className="element-buttons">
             <button onClick={() => handleSizeChange(elementRef.current, 10)}>+</button>
@@ -43,19 +44,26 @@ function App() {
     element.style.height = `${newHeight}px`;
   };
 
-  const handleConnectClick = () => {
-    // Add logic to handle the "Connect" button click here
-  };
-
   const handleSimulateClick = () => {
-    const noAnimationElements = document.querySelectorAll(".no-animation");
-    noAnimationElements.forEach((element) => {
-      const goldenDiv = document.createElement("div");
-      goldenDiv.className="goldendiv"
-      element.insertBefore(goldenDiv, element.firstChild);
-      element.insertBefore(goldenDiv, element.firstChild);
-      element.insertBefore(goldenDiv, element.firstChild);
+    const connectorElement = document.querySelectorAll(".connector");
+    const verticalConnectorElement = document.querySelectorAll(".tbconnector");
+    const verticalBTConnectorElement = document.querySelectorAll(".btconnector")
+    connectorElement.forEach((element) => {
+      let Div = document.createElement("div");
+      Div.className="goldendiv"
+      element.insertBefore(Div, element.firstChild);
     });
+    verticalConnectorElement.forEach((element) => {
+      let Div = document.createElement("div");
+      Div.className="goldendivVer"
+      element.insertBefore(Div, element.secondChild);
+    });
+    verticalBTConnectorElement.forEach((element) => {
+      let Div = document.createElement("div");
+      Div.className="goldendivVerBT"
+      element.insertBefore(Div, element.secondChild);
+    });
+    
   };
 
   return (
@@ -65,7 +73,8 @@ function App() {
         <button onClick={handleButtonClick}>Crane</button>
         <button onClick={handleButtonClick}>Compressor</button>
         <button onClick={handleButtonClick}>Connector</button>
-        <button onClick={handleButtonClick}>Vertical Connector</button>
+        <button onClick={handleButtonClick}>TBconnector</button>
+        <button onClick={handleButtonClick}>BTconnector</button>
         <button onClick={handleButtonClick}>Pump</button>
         <button onClick={handleButtonClick}>Generator</button>
       </div>
